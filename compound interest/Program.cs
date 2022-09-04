cb558c455977d4b733af5a74bb1095447b03d5b5
odewumivictor <vickidubi1@gmail.com>
2022-09-04 17:49:53 +0100

    Add project files.

---
+++ compound interest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace compound_interest
+{
+    internal class compound
+    {
+        public static void Main()
+        {
+            double principle = 10000, rate = 5, time = 2;
+
+            double A = principle * (Math.Pow((1 +
+                        rate / 100), time));
+            double CI = A - principle;
+
+            Console.Write("Compound Interest is " + CI);
+            Console.ReadLine();
+        }
+       
+    }
